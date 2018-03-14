c743bb3450eca09b89a201887ff85f11ca5292ab
Douglas Joseph Pecot <djpecot@gmail.com>
2018-03-14 18:45:52 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+array = [];
+
+function forLoop(array){
+for (let i = 0; i < 25; i++) {
+  if (i == 1){
+    debugger;
+  array = [...array,"I am 1 strange loop"]
+  } else { (i!==1)
+  array = [...array, 'I am ${i} strange loops']
+  }
+}
+return array
+}
